e65084b39858255dbbc80d5f101047b2339352e5
Forrest <fcrawford1992@gmail.com>
2024-10-21 06:52:24 -0400

    Add project files.

---
+++ Direct2DDxfViewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Direct2DDxfViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
