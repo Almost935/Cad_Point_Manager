ca1b15b265dd90ef8658f58942abeb83320e27fd
Forrest Crawford <FCrawford1992@gmail.com>
2024-10-21 20:55:31 -0400

    asdf

---
+++ Cad_Point_Manager/Views/MainView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using netDxf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Cad_Point_Manager.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Page
+    {
+        private string dxfFilePath = @"C:\Users\fcraw\source\repos\Cad-Point-Manager\CadPointManager\Resources\DXF\SmallDxf.dxf";
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            DxfDocument dxfDoc = DxfDocument.Load(dxfFilePath);
+            if (dxfDoc is not null)
+            {
+                dxfViewer.LoadDxfControl(dxfDoc);
+            }
+        }
+    }
+}
