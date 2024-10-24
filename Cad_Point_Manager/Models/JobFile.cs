d4d32901512aa3ab73018a77da22eb2e274b5e8f
Forrest Crawford <FCrawford1992@gmail.com>
2024-10-23 23:39:51 -0400

    10.23.24

---
+++ Cad_Point_Manager/Models/JobFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cad_Point_Manager.Models
+{
+    public class JobFile : BaseModel
+    {
+        #region Fields
+        private string _dxfFilePath;
+        #endregion
+
+        #region Properties
+        public string DxfFilePath
+        {
+            get { return _dxfFilePath; }
+            set
+            {
+                _dxfFilePath = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
