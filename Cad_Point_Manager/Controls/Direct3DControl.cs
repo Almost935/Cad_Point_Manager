506a441caa3826387969cef67893f281eb3d3bef
Forrest Crawford <FCrawford1992@gmail.com>
2024-11-14 21:52:10 -0500

    Todo va cambiando.

--- Cad_Point_Manager/Controls/Direct3DControl.cs
+++ Cad_Point_Manager/Controls/Direct3DControl.cs
@@ -1,30 +1,31 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using SharpDX;
-using SharpDX.Direct3D11;
-using SharpDX.Mathematics.Interop;
-using Device = SharpDX.Direct3D11.Device;
-using Format = SharpDX.DXGI.Format;
 using System.Windows.Media;
-using System.Windows.Threading;
+using System.Windows.Shapes;
+using SharpDX;
 using SharpDX.DXGI;
-using SharpDX.Direct3D;
+using SharpDX.Mathematics.Interop;
+using System.Collections.Generic;
+using System.Linq;
+using SharpDX.Direct3D9;
 
 namespace Cad_Point_Manager.Controls
 {
     public class Direct3DControl : UserControl
     {
-        private Device device;
-        private DeviceContext context;
-        private SwapChain swapChain;
         private RenderTargetView renderTargetView;
-        private bool isRendering;
-        private Task renderTask;
-        private CancellationTokenSource cancellationTokenSource;
-        private DispatcherTimer renderTimer;
+        private Device device;
+        private SwapChain swapChain;
+        private DeviceContext context;
+        private RenderTarget2D renderTarget;
+        private SharpDX.Direct3D11.Buffer vertexBuffer;
+        private ShaderBytecode vertexShaderBytecode;
+        private ShaderBytecode pixelShaderBytecode;
+        private VertexShader vertexShader;
+        private PixelShader pixelShader;
+        private List<Line> lines = new List<Line>();
+        private List<Arc> arcs = new List<Arc>();
 
         public Direct3DControl()
         {
@@ -35,38 +36,145 @@ namespace Cad_Point_Manager.Controls
         private void Direct3DControl_Loaded(object sender, RoutedEventArgs e)
         {
             InitializeDirect3D();
-            StartRenderingAsync();
         }
 
         private void Direct3DControl_Unloaded(object sender, RoutedEventArgs e)
         {
-            StopRendering();
-            CleanupDirect3D();
+            Dispose(true);
         }
 
-        private void InitializeDirec3D()
+        private void InitializeDirect3D()
         {
-            var swapChainDesc = new SwapChainDescription
+            // Create Device and SwapChain
+            var factory = new Factory1();
+            var description = new SwapChainDescription
             {
                 BufferCount = 1,
-                ModeDescription = new ModeDescription((int)this.ActualWidth, (int)this.ActualHeight, new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                ModeDescription = new ModeDescription(Width, Height, new Rational(60, 1), Format.R8G8B8A8_UNorm),
                 IsWindowed = true,
-                OutputHandle = new System.IntPtr(),
+                OutputHandle = this.GetWindowHandle(),
                 SampleDescription = new SampleDescription(1, 0),
                 SwapEffect = SwapEffect.Discard,
                 Usage = Usage.RenderTargetOutput
             };
-
-            // Create the Direct3D device and context
-            device = new Device(DriverType.Hardware, DeviceCreationFlags.None);
+            swapChain = new SwapChain(factory, new Device(DriverType.Hardware), DeviceCreationFlags.None, description);
+            device = swapChain.Device;
             context = device.ImmediateContext;
 
-            var factory = new Factory1();
-            swapChain = new SwapChain(factory, device, swapChainDesc);
+            // Create Render Target
             var backBuffer = swapChain.GetBackBuffer<Texture2D>(0);
             renderTargetView = new RenderTargetView(device, backBuffer);
-
             context.OutputMerger.SetRenderTargets(renderTargetView);
+
+            // Set up shaders
+            var vertexShaderSource = ShaderBytecode.CompileFromFile("vertexShader.hlsl", "main", "vs_4_0");
+            var pixelShaderSource = ShaderBytecode.CompileFromFile("pixelShader.hlsl", "main", "ps_4_0");
+
+            vertexShaderBytecode = vertexShaderSource;
+            pixelShaderBytecode = pixelShaderSource;
+
+            vertexShader = new VertexShader(device, vertexShaderBytecode);
+            pixelShader = new PixelShader(device, pixelShaderBytecode);
+
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+            // Create buffers
+            CreateBuffers();
+
+            // Set up the view
+            this.SizeChanged += (s, e) => { if (renderTargetView != null) renderTargetView.Dispose(); InitializeDirect3D(); };
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            // Clear screen
+            context.ClearRenderTargetView(renderTargetView, new RawColor4(0.0f, 0.0f, 0.0f, 1.0f));
+
+            // Draw lines and arcs
+            DrawLinesAndArcs();
+
+            // Present the result
+            swapChain.Present(0, PresentFlags.None);
+        }
+
+        private void DrawLinesAndArcs()
+        {
+            // Set up primitive topology
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.LineList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertexBuffer, SharpDX.Utilities.SizeOf<LineVertex>(), 0));
+
+            // Draw the lines
+            context.Draw(lines.Count * 2, 0);
+
+            // Additional logic for arcs or other 2D primitives can go here...
+        }
+
+        private void CreateBuffers()
+        {
+            // Example: Create a simple line vertex buffer
+            var vertices = new[]
+            {
+                new LineVertex(new Vector3(100, 100, 0)),
+                new LineVertex(new Vector3(200, 200, 0)),
+                new LineVertex(new Vector3(200, 100, 0)),
+                new LineVertex(new Vector3(300, 200, 0))
+            };
+
+            vertexBuffer = new SharpDX.Direct3D11.Buffer(device, new DataStream(vertices, true, true), new BufferDescription
+            {
+                SizeInBytes = (int)(vertices.Length * SharpDX.Utilities.SizeOf<LineVertex>()),
+                Usage = ResourceUsage.Default,
+                BindFlags = BindFlags.VertexBuffer,
+                CpuAccessFlags = CpuAccessFlags.None
+            });
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                vertexBuffer?.Dispose();
+                vertexShader?.Dispose();
+                pixelShader?.Dispose();
+                renderTargetView?.Dispose();
+                swapChain?.Dispose();
+                device?.Dispose();
+            }
+        }
+
+        private IntPtr GetWindowHandle()
+        {
+            // To get the window handle, which is necessary for Direct3D swap chain.
+            // For WPF, you can use the following code:
+            var hwndSource = PresentationSource.FromVisual(this) as HwndSource;
+            return hwndSource?.Handle ?? IntPtr.Zero;
+        }
+
+        public class Line
+        {
+            public Vector2 Start { get; set; }
+            public Vector2 End { get; set; }
+        }
+
+        public class Arc
+        {
+            public Vector2 Center { get; set; }
+            public float Radius { get; set; }
+            public float StartAngle { get; set; }
+            public float EndAngle { get; set; }
+        }
+
+        public struct LineVertex
+        {
+            public Vector3 Position;
+
+            public LineVertex(Vector3 position)
+            {
+                Position = position;
+            }
         }
     }
 }
